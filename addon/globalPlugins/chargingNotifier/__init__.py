0d916264ff6ad50bbb6de20aef0f9bd85a9cb2c5
cary-rowen <manchen_0528@outlook.com>
2025-12-25 23:57:53 +0800

    Initial version.

---
+++ addon/globalPlugins/chargingNotifier/__init__.py
@@ -0,0 +1,101 @@
+# chargingNotifier add-on for NVDA
+# This file is covered by the GNU General Public License.
+# See the file COPYING.txt for more details.
+# Copyright (C) 2025 Cary-rowen <manchen_0528@outlook.com>
+
+"""
+Replaces NVDA's voice announcements for AC power status changes
+with sound notifications.
+"""
+
+import os
+from enum import IntEnum
+
+import addonHandler
+import config
+import globalPluginHandler
+import gui
+import nvwave
+from winAPI import _powerTracking
+from winAPI._powerTracking import PowerState, _ReportContext
+
+from . import settings
+
+try:
+	addonHandler.initTranslation()
+except addonHandler.AddonError:
+	pass
+
+
+class NotificationType(IntEnum):
+	"""Notification type for charging status changes."""
+	VOICE = 0
+	SOUND = 1
+
+
+# Configuration specification
+CONFSPEC = {
+	"notificationType": f"integer(default={NotificationType.VOICE}, min=0, max=1)",
+}
+config.conf.spec["chargingNotifier"] = CONFSPEC
+
+# Store original function for restoration
+_originalReportPowerStatus = _powerTracking._reportPowerStatus
+
+
+def _getSoundPath(name: str) -> str:
+	"""Return the full path to a sound file."""
+	return os.path.join(os.path.dirname(__file__), "sounds", f"{name}.wav")
+
+
+def _patchedReportPowerStatus(context: _ReportContext) -> None:
+	"""
+	Patched power status reporter that plays sounds for AC changes
+	when configured to do so.
+	"""
+	# Only intercept AC status changes
+	if context != _ReportContext.AC_STATUS_CHANGE:
+		_originalReportPowerStatus(context)
+		return
+
+	notificationType = config.conf["chargingNotifier"]["notificationType"]
+
+	# Voice mode: delegate to original
+	if notificationType == NotificationType.VOICE:
+		_originalReportPowerStatus(context)
+		return
+
+	# Sound mode: play appropriate sound
+	status = _powerTracking._getPowerStatus()
+	if status is None:
+		return
+
+	currentState = status.ACLineStatus
+	previousState = _powerTracking._powerState
+
+	if currentState != previousState:
+		if currentState == PowerState.AC_ONLINE:
+			nvwave.playWaveFile(_getSoundPath("connected"))
+		elif currentState == PowerState.AC_OFFLINE:
+			nvwave.playWaveFile(_getSoundPath("disconnected"))
+
+	# Update power state
+	_powerTracking._powerState = currentState
+
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	"""Global plugin for charging status sound notifications."""
+
+	def __init__(self):
+		super().__init__()
+		gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(
+			settings.ChargingNotifierPanel
+		)
+		_powerTracking._reportPowerStatus = _patchedReportPowerStatus
+
+	def terminate(self):
+		super().terminate()
+		gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(
+			settings.ChargingNotifierPanel
+		)
+		_powerTracking._reportPowerStatus = _originalReportPowerStatus
---
+++ addon/globalPlugins/chargingNotifier/settings.py
@@ -0,0 +1,47 @@
+# chargingNotifier add-on for NVDA
+# This file is covered by the GNU General Public License.
+# See the file COPYING.txt for more details.
+# Copyright (C) 2025 Cary-rowen <manchen_0528@outlook.com>
+
+"""Settings panel for charging notifier preferences."""
+
+import addonHandler
+import config
+import gui
+import wx
+from gui import guiHelper
+
+try:
+	addonHandler.initTranslation()
+except addonHandler.AddonError:
+	pass
+
+
+class ChargingNotifierPanel(gui.settingsDialogs.SettingsPanel):
+	"""Settings panel for configuring charging status notifications."""
+
+	# Translators: Title of the Charging Notifier settings panel.
+	title = _("Charging Notifier")
+
+	def makeSettings(self, settingsSizer):
+		helper = guiHelper.BoxSizerHelper(self, sizer=settingsSizer)
+
+		# Translators: Label for the notification type combo box.
+		label = _("Charging status notification type:")
+		# Translators: Voice notification option.
+		# Translators: Sound notification option.
+		choices = [_("Voice"), _("Sound")]
+
+		self._notificationTypeChoice = helper.addLabeledControl(
+			label,
+			wx.Choice,
+			choices=choices,
+		)
+		self._notificationTypeChoice.SetSelection(
+			config.conf["chargingNotifier"]["notificationType"]
+		)
+
+	def onSave(self):
+		config.conf["chargingNotifier"]["notificationType"] = (
+			self._notificationTypeChoice.GetSelection()
+		)
