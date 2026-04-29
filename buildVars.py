2a216fda47c5dabce33ee64d29c418270d8dd7d6
cary-rowen <manchen_0528@outlook.com>
2026-04-30 07:38:30 +0800

    Release 0.2.2

--- buildVars.py
+++ buildVars.py
@@ -24,10 +24,10 @@ addon_info = AddonInfo(
 	addon_description=_("""An add-on for NVDA that allows you to customize AC power status alerts
 using voice announcements, sound notifications, or both."""),
 	# version
-	addon_version="0.2.1",
+	addon_version="0.2.2",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""Initial version"""),
+	addon_changelog=_("""Declare compatibility with NVDA 2026.1."""),
 	# Author(s)
 	addon_author="Cary-rowen <manchen_0528@outlook.com>",
 	# URL for the add-on documentation support
@@ -39,7 +39,7 @@ using voice announcements, sound notifications, or both."""),
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
 	addon_minimumNVDAVersion="2023.1.0",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	addon_lastTestedNVDAVersion="2025.1.0",
+	addon_lastTestedNVDAVersion="2026.1.0",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+## 0.2.2
+
+* Declare compatibility with NVDA 2026.1.
+
 ## 0.2.0
 
 **Voice and Sound**: Provides both audio cues and voice announcements.
